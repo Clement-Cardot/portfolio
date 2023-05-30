6644bb60071e8c891d4ce0fcaf9c150ceb0fc956
Clément CARDOT <clementcdt25@gmail.com>
2023-05-30 13:12:11 +0200

    define model for project

--- src/app/components/model/project.model.ts
+++ src/app/components/model/project.model.ts
@@ -1,2 +1,19 @@
 export class Project {
+    id: number;
+    title: string;
+    description: string;
+    images: string[];
+    status: string;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(id: number, title: string, description: string, images:string[], status: string, created_at: Date, updated_at: Date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.images = images;
+        this.status = status;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
 }
